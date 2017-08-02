0a999040618db54597dc3595529a96a0cc2bd0c4
RCXcrafter <rcxcrafter@gmail.com>
2017-08-02 20:29:29 +0200

    fix server crash

--- src/main/java/teamroots/embers/entity/EntityEmberProjectile.java
+++ src/main/java/teamroots/embers/entity/EntityEmberProjectile.java
@@ -19,20 +19,14 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.common.Optional.Interface;
-import net.minecraftforge.fml.common.Optional.Method;
-import teamroots.embers.EventManager;
 import teamroots.embers.RegistryManager;
-import teamroots.embers.network.PacketHandler;
-import teamroots.embers.network.message.MessageEmberSizedBurstFX;
 import teamroots.embers.particle.ParticleUtil;
 
 //@Interface(iface = "elucent.albedo.lighting.ILightProvider", modid = "albedo")
 public class EntityEmberProjectile extends Entity/* implements ILightProvider*/ {
     public static final DataParameter<Float> value = EntityDataManager.<Float>createKey(EntityEmberProjectile.class, DataSerializers.FLOAT);
-    public static final DataParameter<Boolean> dead = EntityDataManager.<Boolean>createKey(EntityEmberProjectile.class, DataSerializers.BOOLEAN);
-    public static final DataParameter<Integer> lifetime = EntityDataManager.<Integer>createKey(EntityEmberProjectile.class, DataSerializers.VARINT);
 	BlockPos pos = new BlockPos(0,0,0);
+	int lifetime = 160;
 	public BlockPos dest = new BlockPos(0,0,0);
 	public UUID id = null;
 	
@@ -40,8 +34,6 @@ public class EntityEmberProjectile extends Entity/* implements ILightProvider*/ 
 		super(worldIn);
 		this.setInvisible(true);
 		this.getDataManager().register(value, Float.valueOf(0));
-		this.getDataManager().register(dead, false);
-		this.getDataManager().register(lifetime, Integer.valueOf(160));
 	}
 	
 	public void initCustom(double x, double y, double z, double vx, double vy, double vz, double value, UUID playerId){
@@ -83,78 +75,66 @@ public class EntityEmberProjectile extends Entity/* implements ILightProvider*/ 
 	@Override
 	public void onUpdate(){
 		super.onUpdate();
-		if (!getEntityWorld().isRemote && getDataManager().get(lifetime) > 18 && getDataManager().get(dead)){
-			PacketHandler.INSTANCE.sendToAll(new MessageEmberSizedBurstFX(posX, posY, posZ,getDataManager().get(value)/1.75f));
-		}
-		getDataManager().set(lifetime, getDataManager().get(lifetime)-1);
-		getDataManager().setDirty(lifetime);
-		if (getDataManager().get(lifetime) <= 0){
+		lifetime --;
+		if (lifetime <= 0){
 			getEntityWorld().removeEntity(this);
 			this.kill();
 		}
-		if (!getDataManager().get(dead)){
-			getDataManager().set(value, getDataManager().get(value)-0.025f);
-			if (getDataManager().get(value) <= 0){
-				getEntityWorld().removeEntity(this);
-			}
-			
-			posX += motionX;
-			posY += motionY;
-			posZ += motionZ;
-			IBlockState state = getEntityWorld().getBlockState(getPosition());
-			if (state.isFullCube() && state.isOpaqueCube()){
-				if (!getEntityWorld().isRemote){
-					PacketHandler.INSTANCE.sendToAll(new MessageEmberSizedBurstFX(posX, posY, posZ,getDataManager().get(value)/1.75f));
-					getDataManager().set(lifetime, 20);
-					getDataManager().setDirty(lifetime);
-					this.getDataManager().set(dead, true);
-					getDataManager().setDirty(dead);
-				}
-			}
+		getDataManager().set(value, getDataManager().get(value)-0.025f);
+		if (getDataManager().get(value) <= 0){
+			getEntityWorld().removeEntity(this);
+			this.kill();
+		}
+		
+		posX += motionX;
+		posY += motionY;
+		posZ += motionZ;
+		IBlockState state = getEntityWorld().getBlockState(getPosition());
+		if (state.isFullCube() && state.isOpaqueCube()){
 			if (getEntityWorld().isRemote){
-				for (double i = 0; i < 9; i ++){
-					double coeff = i/9.0;
-					ParticleUtil.spawnParticleGlow(getEntityWorld(), (float)(prevPosX+(posX-prevPosX)*coeff), (float)(prevPosY+(posY-prevPosY)*coeff), (float)(prevPosZ+(posZ-prevPosZ)*coeff), 0.0125f*(rand.nextFloat()-0.5f), 0.0125f*(rand.nextFloat()-0.5f), 0.0125f*(rand.nextFloat()-0.5f), 255, 64, 16, getDataManager().get(value)/1.75f, 24);
+				for (int i = 0; i < 80; i ++){
+					ParticleUtil.spawnParticleGlow(getEntityWorld(), (float)posX, (float)posY, (float)posZ, 0.25f*(rand.nextFloat()-0.5f), 0.25f*(rand.nextFloat()-0.5f), 0.25f*(rand.nextFloat()-0.5f), 255, 64, 16, getDataManager().get(value)/1.75f, 24);
 				}
 			}
-			List<EntityLivingBase> rawEntities = getEntityWorld().getEntitiesWithinAABB(EntityLivingBase.class, new AxisAlignedBB(posX-getDataManager().get(value)*0.125,posY-getDataManager().get(value)*0.125,posZ-getDataManager().get(value)*0.125,posX+getDataManager().get(value)*0.125,posY+getDataManager().get(value)*0.125,posZ+getDataManager().get(value)*0.125));
-			ArrayList<EntityLivingBase> entities = new ArrayList<EntityLivingBase>();
-			for (int j = 0; j < rawEntities.size(); j ++){
-				if (id != null){
-					if (rawEntities.get(j).getUniqueID().compareTo(id) != 0){
-						entities.add(rawEntities.get(j));
-					}
-				}
+			getEntityWorld().removeEntity(this);
+			this.kill();
+		}
+		if (getEntityWorld().isRemote){
+			for (double i = 0; i < 9; i ++){
+				double coeff = i/9.0;
+				ParticleUtil.spawnParticleGlow(getEntityWorld(), (float)(prevPosX+(posX-prevPosX)*coeff), (float)(prevPosY+(posY-prevPosY)*coeff), (float)(prevPosZ+(posZ-prevPosZ)*coeff), 0.0125f*(rand.nextFloat()-0.5f), 0.0125f*(rand.nextFloat()-0.5f), 0.0125f*(rand.nextFloat()-0.5f), 255, 64, 16, getDataManager().get(value)/1.75f, 24);
 			}
-			if (entities.size() > 0){
-				for (EntityLivingBase target : entities){
-					DamageSource source = RegistryManager.damage_ember;
-					if (getEntityWorld().getPlayerEntityByUUID(id) != null){
-						EntityPlayer player = getEntityWorld().getPlayerEntityByUUID(id);
-						source = DamageSource.causePlayerDamage(player);
-						target.setFire(1);
-						target.attackEntityFrom(source, getDataManager().get(value));
-						target.setLastAttacker(player);
-						target.setRevengeTarget(player);
-						target.knockBack(this, 0.5f, -motionX, -motionZ);
-					}
-					else {
-						target.attackEntityFrom(source, getDataManager().get(value));
-					}
-					if (!getEntityWorld().isRemote){
-						PacketHandler.INSTANCE.sendToAll(new MessageEmberSizedBurstFX(posX, posY, posZ,getDataManager().get(value)/1.75f));
-						getDataManager().set(lifetime, 20);
-						getDataManager().setDirty(lifetime);
-						this.getDataManager().set(dead, true);
-						getDataManager().setDirty(dead);
-					}
+		}
+		List<EntityLivingBase> rawEntities = getEntityWorld().getEntitiesWithinAABB(EntityLivingBase.class, new AxisAlignedBB(posX-getDataManager().get(value)*0.125,posY-getDataManager().get(value)*0.125,posZ-getDataManager().get(value)*0.125,posX+getDataManager().get(value)*0.125,posY+getDataManager().get(value)*0.125,posZ+getDataManager().get(value)*0.125));
+		ArrayList<EntityLivingBase> entities = new ArrayList<EntityLivingBase>();
+		for (int j = 0; j < rawEntities.size(); j ++){
+			if (id != null){
+				if (rawEntities.get(j).getUniqueID().compareTo(id) != 0){
+					entities.add(rawEntities.get(j));
 				}
 			}
 		}
-		else {
-			motionX = 0;
-			motionY = 0;
-			motionZ = 0;
+		if (entities.size() > 0){
+			EntityLivingBase target = entities.get(0);
+			DamageSource source = RegistryManager.damage_ember;
+			if (getEntityWorld().getPlayerEntityByUUID(id) != null){
+				EntityPlayer player = getEntityWorld().getPlayerEntityByUUID(id);
+				source = DamageSource.causePlayerDamage(player);
+				target.setFire(1);
+				target.attackEntityFrom(source, getDataManager().get(value));
+				target.setLastAttacker(player);
+				target.setRevengeTarget(player);
+				target.knockBack(this, 0.5f, -motionX, -motionZ);
+			}
+			else {
+				target.attackEntityFrom(source, getDataManager().get(value));
+			}
+			if (getEntityWorld().isRemote){
+				for (int i = 0; i < 80; i ++){
+					ParticleUtil.spawnParticleGlow(getEntityWorld(), (float)posX, (float)posY, (float)posZ, 0.25f*(rand.nextFloat()-0.5f), 0.25f*(rand.nextFloat()-0.5f), 0.25f*(rand.nextFloat()-0.5f), 255, 64, 16, getDataManager().get(value)/1.75f, 24);
+				}
+			}
+			this.setDead();
 		}
 	}
 
